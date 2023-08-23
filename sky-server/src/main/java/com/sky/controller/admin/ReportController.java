78c378124efe8b73a3b50d6a28715e033469b510
itly <itly@itcast.cn>
2023-08-23 23:49:44 +0800

    导出运营数据报表

--- sky-server/src/main/java/com/sky/controller/admin/ReportController.java
+++ sky-server/src/main/java/com/sky/controller/admin/ReportController.java
@@ -15,6 +15,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.HttpServletResponse;
 import java.time.LocalDate;
 
 @RestController
@@ -85,6 +86,12 @@ public class ReportController {
     @DateTimeFormat(pattern = "yyyy-MM-dd") LocalDate end){
         log.info("销量前十排行：{},{}", begin, end);
         return Result.success(reportService.getOrderSalesTop10(begin, end));
+    }
+
+    @GetMapping("/export")
+    @ApiOperation("导出运营数据报表")
+    public void export(HttpServletResponse response){
+        reportService.exprotBusinessData(response);
     }
 
 }
